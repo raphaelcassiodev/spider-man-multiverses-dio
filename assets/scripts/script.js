86c4009feb92c21b2f624b8175d59176d9118ecd
raphaelcassiodev <raphael.cassiodev@outlook.com>
2024-12-14 03:31:10 -0300

    meu-projeto-spider-man

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,36 +1,36 @@
+const cards = document.querySelectorAll(".s-card");
+const bgCards = document.querySelectorAll(".s-card__background");
+
 function handleMouseEnter() {
-  this.classList.add('s-card--hovered');
+  this.classList.add("s-card--hovered");
   document.body.id = `${this.id}-hovered`;
 }
 
-function handleMouseLeave() {
-  this.classList.remove('s-card--hovered');
+function hendleMouseLeave(){
+  this.classList.remove('s-card--hovered')
   document.body.id = '';
+
 }
 
-function addEventListenersToCards() {
-  const cardElements = document.getElementsByClassName('s-card');
-  
-  for (let index = 0; index < cardElements.length; index++) {
-    const card = cardElements[index];
-    card.addEventListener('mouseenter', handleMouseEnter);
-    card.addEventListener('mouseleave', handleMouseLeave);
-  }
-}
+cards.forEach((img) => {
+  img.addEventListener("mouseenter", handleMouseEnter)
+  img.addEventListener("mouseleave",hendleMouseLeave)
+});
 
-document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
 
-function selectCarouselItem(selectedButtonElement) {
-  const selectedItem = selectedButtonElement.id;
+function selectCarouselItem(element){
+  const selectedItem = element.id;
   const carousel = document.querySelector('.s-cards-carousel');
-  const transform = carousel.style.transform;
-  const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
-  const rotateYDeg = -120 * (Number(selectedItem) - 1);
-  const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
+  const transformValue = carousel.style.transform;
+  const rotateY = transformValue.match(/rotateY\((-?\d+deg)\)/i); 
+  const rotateYDeg = -120 * (Number(selectedItem) - 1); 
+  const newTransform = transformValue.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`)
 
   carousel.style.transform = newTransform;
 
   const activeButtonElement = document.querySelector('.s-controller__button--active');
+
   activeButtonElement.classList.remove('s-controller__button--active');
-  selectedButtonElement.classList.add('s-controller__button--active');
+  element.classList.add('s-controller__button--active')
+ 
 }
